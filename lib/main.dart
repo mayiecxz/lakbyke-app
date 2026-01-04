423cb1fca9b3eb0e77fe6532ddc18f623106335d
Lakbyke Developer <developer@lakbyke.local>
2026-01-04 22:12:44 +0800

    auth service for login/signup logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lakbyke_mobile/utils/theme.dart';
 import 'package:lakbyke_mobile/screens/onboarding/onboarding_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
